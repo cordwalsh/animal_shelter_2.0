12794f0a7ffd9147c8f73e1b70625b0326109675
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-08-23 16:23:16 -0700

    reconfig rspec, still working

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
 
-  factory :pets do
+  factory :pet do
     type_of_animal { Faker::Creature::Animal.name }
     name  { Faker::Creature::Dog.name }
     age  { rand(1..15) }
     gender  { Faker::Creature::Dog.gender }
   end
 
-  after(:create) do |pets|
-     create_list(:type_of_animal => type_of_animal, :name => name, :age => age, :gender => gender)
-  end
+  # after(:create) do |pets|
+  #    create_list(:type_of_animal => type_of_animal, :name => name, :age => age, :gender => gender)
+  # end
 
 end
